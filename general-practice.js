5c7ada18aa4d6bfc0f683b95633f975241b07a9c
Tobin Waters <tkwaterss@gmail.com>
2023-02-05 09:59:33 -0700

    Intitialized Practice

---
+++ general-practice.js
@@ -0,0 +1,25 @@
+//.forEach() coded out
+const forEach = () => {
+
+}
+
+
+
+//.filter() coded out
+const filter = () => {
+
+}
+
+
+//.map() coded out
+const map = () => {
+
+}
+
+
+
+//.reduce() coded out
+const reduce = () => {
+    
+}
+
