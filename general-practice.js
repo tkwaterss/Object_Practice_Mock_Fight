6ed497875dd111357a0e76b458c34e198df103e7
Tobin Waters <tkwaterss@gmail.com>
2023-02-05 10:04:56 -0700

    Added forEach Code

--- general-practice.js
+++ general-practice.js
@@ -1,25 +1,27 @@
-//.forEach() coded out
-const forEach = () => {
-
+//.forEach() - function that will perform a tast for each given element in the array. does not modify array.
+const forEach = (array, callback) => {
+    for (let i = 0; i < array.length; i++) {
+        return callback(array[i]);
+    }
 }
 
 
 
-//.filter() coded out
-const filter = () => {
+//.filter() - function will return a new array copy filtered via the callback Function
+const filter = (array, callback) => {
 
 }
 
 
-//.map() coded out
-const map = () => {
+//.map() -
+const map = (array, callback) => {
 
 }
 
 
 
 //.reduce() coded out
-const reduce = () => {
+const reduce = (array, callback) => {
     
 }
 
